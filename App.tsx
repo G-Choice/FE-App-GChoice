3acc67b914432d22d019b2034ac29ce1dbf84f57
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2024-01-21 00:36:54 +0700

    GC-35: Done build UI for register

--- App.tsx
+++ App.tsx
@@ -1,24 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {Home} from "./src/screens/home";
 import {BottomTabs} from "./src/navigations";
+import { RegisterLayout } from './src/screens/register/RegisterLayout';
+import SplashScreen from './src/screens/slash/SplashScreen';
+import { Verification } from './src/screens/verification/Verification';
+
+
 
 export const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen
+          options={{ title: '', headerShown: false }}
+          name='SplashScreen'
+          component={SplashScreen}
+        />
         <Stack.Screen options={{title: '', headerShown: false}} name='BottomTabs' component={BottomTabs} />
         {/*<Stack.Screen options={{title: '', headerShown: false}} name='HomeScreen' component={Home} />*/}
+        <Stack.Screen options={{title: '', headerShown: false}} name='RegisterScreen' component={RegisterLayout} />
+        <Stack.Screen options={{title: '', headerShown: false}} name='VerificationScreen' component={Verification} />
+
       </Stack.Navigator>
     </NavigationContainer>
   )
