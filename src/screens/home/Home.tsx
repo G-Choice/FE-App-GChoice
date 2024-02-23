68f7afb51b2a22bb3a34f72327632726962fb230
vinhuyen.dev <vinhuyen.theonly@gmail.com>
2024-02-23 15:18:53 +0700

    fix: did not display product

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,34 +1,48 @@
-import {Dimensions, StyleSheet} from "react-native";
-import {Header} from "../../components/child";
-import {Colors} from "../../assets/colors";
-import {CardContainer, Deal} from "../../components/card";
-import PromotionSlider from "../../components/card/PromotionSlider.tsx";
-import {ScrollView} from 'react-native-virtualized-view'
-import {FlashSaleCardContainer} from "../../components/card/FlashSaleCardContainer.tsx";
-import {SearchBar} from "../../components/input/SearchBar.tsx";
 import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Header } from "../../components/child";
+import { Colors } from "../../assets/colors";
+import { CardContainer } from "../../components/card";
+import PromotionSlider from "../../components/card/PromotionSlider.tsx";
+import { ScrollView } from "react-native-virtualized-view";
+import { SearchBar } from "../../components/input/SearchBar.tsx";
 
-const {width} = Dimensions.get("screen")
+const { width } = Dimensions.get("screen");
 
 const Home = () => {
-  const handleBack = () => {}
-  const handleSearch = () => {}
+  const handleBack = () => {};
+  const handleSearch = () => {};
   return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      <Header />
-      <SearchBar placeholder="Search..." onSubmit={handleSearch} onBack={handleBack} />
-      <PromotionSlider />
-      {/*<Deal />*/}
-      <CardContainer />
-    </ScrollView>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.stickyHeader}>
+        <Header />
+        <SearchBar placeholder="Search..." onSubmit={handleSearch} onBack={handleBack} />
+      </View>
+      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+        <PromotionSlider />
+        {/*<Deal />*/}
+        <CardContainer />
+      </ScrollView>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.semiLightGrey,
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+  stickyHeader: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    elevation: 2,
+  },
+  scrollView: {
+    marginTop: 100, // Adjust this value according to the height of the sticky header
+  },
+});
 
-export {Home};
+export { Home };
