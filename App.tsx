d93e464e552c5b49a9854e897d722d9fe8aa5bf8
nguyenthiutvien <ntuv003@gmail.com>
2024-02-26 08:10:01 +0700

    GC-29 feat/done product detail

--- App.tsx
+++ App.tsx
@@ -1,55 +1,3 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  */
-
-// import React from 'react';
-// import {NavigationContainer} from "@react-navigation/native";
-// import {createNativeStackNavigator} from "@react-navigation/native-stack";
-// import {Home} from "./src/screens/home";
-// import {BottomTabs} from "./src/navigations";
-// // import { RegisterLayout } from './src/screens/register/RegisterLayout';
-// // import SplashScreen from './src/screens/slash/SplashScreen';
-// // import { Verification } from './src/screens/verification/Verification';
-// // import {LoginLayout} from './src/screens/login/LoginLayout';
-// import {
-//   RegisterLayout,
-//   SplashScreen,
-//   Verification,
-//   LoginLayout,
-//   ProductDetail,
-// } from './src/screens';
-// import {store} from './src/app/store'
-// import {Provider} from 'react-redux'
-
-
-// export const Stack = createNativeStackNavigator();
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="SplashScreen">
-//         <Stack.Screen
-//           options={{ title: '', headerShown: false }}
-//           name='SplashScreen'
-//           component={SplashScreen}
-//         />
-//         <Stack.Screen options={{title: '', headerShown: false}} name='RegisterScreen' component={RegisterLayout} />
-//         <Stack.Screen options={{title: '', headerShown: false}} name='VerificationScreen' component={Verification} />
-//         <Stack.Screen options={{title: '', headerShown: false}} name='LoginScreen' component={LoginLayout} />
-//         <Stack.Screen options={{title: '', headerShown: true}} name='ProductDetail' component={ProductDetail} />
-//         <Stack.Screen options={{title: '', headerShown: false}} name='BottomTabs' component={BottomTabs} />
-//         <Stack.Screen options={{title: '', headerShown: false}} name='HomeScreen' component={Home} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//     </Provider>
-//   )
-// }
-
-// export default App;
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -62,10 +10,6 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {Home} from "./src/screens/home";
 import {BottomTabs} from "./src/navigations";
-// import { RegisterLayout } from './src/screens/register/RegisterLayout';
-// import SplashScreen from './src/screens/slash/SplashScreen';
-// import { Verification } from './src/screens/verification/Verification';
-// import {LoginLayout} from './src/screens/login/LoginLayout';
 import {
   RegisterLayout,
   SplashScreen,
@@ -92,7 +36,6 @@ const AuthStack = () => {
 };
 
 const MainStack = () => {
-  // const { authToken, refreshToken } = useSelector((state: RootState) => state.auth);
   return (
     <Stack.Navigator>
       <Stack.Screen options={{title: '', headerShown: false}} name='HomeScreen' component={BottomTabs} />
@@ -102,8 +45,6 @@ const MainStack = () => {
 };
 
 const App = () => {
-  //const { authToken, refreshToken } = useSelector((state: RootState) => state.auth);
-  // console.log(auth,'sss')
   // const { authToken } = useSelector((state: RootState) => state.auth);
   // console.log(authToken,'a')
   const authTokens = true
